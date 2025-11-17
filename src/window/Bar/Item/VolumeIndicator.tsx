8b6078a8d52fdb5ebd78219642eee5241af5aee7
Edward Allen <hi@eallen.me>
2025-11-17 15:04:43 +1100

    implement basic volume indicator

--- src/window/Bar/Item/VolumeIndicator.tsx
+++ src/window/Bar/Item/VolumeIndicator.tsx
@@ -1,3 +1,26 @@
+import AstalWp from "gi://AstalWp?version=0.1"
+import { createBinding, createComputed } from "ags"
+import { Config } from "@/config"
+
+const wp = AstalWp.get_default()
+
 export default function VolumeIndicator() {
-  return <image iconName="sy-volume-up-symbolic" pixelSize={20} />
+  const present = createBinding(wp, "defaultSpeaker").as((speaker) => !!speaker)
+  const icon = createComputed([
+    createBinding(wp.defaultSpeaker, "volume"),
+    createBinding(wp.defaultSpeaker, "mute"),
+  ]).as(([volume, mute]) => {
+    if (mute) return "sy-volume-mute-symbolic"
+    if (volume > 0.5) return "sy-volume-up-symbolic"
+    if (volume > 0) return "sy-volume-down-symbolic"
+    return "sy-volume-low-symbolic"
+  })
+
+  return (
+    <image
+      visible={present}
+      iconName={icon}
+      pixelSize={Config.sizing.indicatorIcon}
+    />
+  )
 }
