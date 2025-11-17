49a95518a435b1b0aeb856cfb93a814b4edcb046
Edward Allen <hi@eallen.me>
2025-11-17 15:54:57 +1100

    start building the wallpaper window

--- src/window/Wallpaper.tsx
+++ src/window/Wallpaper.tsx
@@ -1,5 +1,17 @@
-import { Gdk } from "ags/gtk4"
+import { Astal, Gdk } from "ags/gtk4"
 
 export default function Wallpaper(monitor: Gdk.Monitor) {
-  return <window></window>
+  const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
+
+  return (
+    <window
+      visible
+      name="wallpaper"
+      namespace="wallpaper"
+      gdkmonitor={monitor}
+      layer={Astal.Layer.BACKGROUND}
+      exclusivity={Astal.Exclusivity.IGNORE}
+      anchor={TOP | BOTTOM | LEFT | RIGHT}
+    />
+  )
 }
