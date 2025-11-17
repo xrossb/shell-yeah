ff9ea67e24f8fed5fdbfbf580c5be8a0fccb7449
Edward Allen <hi@eallen.me>
2025-11-17 21:15:15 +1100

    wip

--- src/window/Wallpaper.tsx
+++ src/window/Wallpaper.tsx
@@ -1,7 +1,20 @@
-import { Astal, Gdk } from "ags/gtk4"
+import { Config } from "@/config"
+import { Astal, Gdk, Gtk } from "ags/gtk4"
+import { createPoll, Timer } from "ags/time"
+import GdkPixbuf from "gi://GdkPixbuf?version=2.0"
+import Gio from "gi://Gio"
+import { createState } from "gnim"
 
 export default function Wallpaper(monitor: Gdk.Monitor) {
   const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
+
+  const [reveal, setReveal] = createState(false)
+  const wallpaper = new Gtk.Picture({
+    contentFit: Gtk.ContentFit.COVER,
+    canShrink: true,
+    hexpand: true,
+    vexpand: true,
+  })
 
   return (
     <window
@@ -12,6 +25,25 @@ export default function Wallpaper(monitor: Gdk.Monitor) {
       layer={Astal.Layer.BACKGROUND}
       exclusivity={Astal.Exclusivity.IGNORE}
       anchor={TOP | BOTTOM | LEFT | RIGHT}
-    />
+      $={(self) => {
+        const file = Gio.File.new_for_path(`${SRC}/assets/wallpaper.png`)
+        file.load_bytes_async(null, (file, res) => {
+          const [bytes, _] = file!.load_bytes_finish(res)
+          const loader = new GdkPixbuf.PixbufLoader()
+          loader.write_bytes(bytes)
+          loader.close()
+          wallpaper.set_pixbuf(loader.get_pixbuf())
+          setReveal(true)
+        })
+      }}
+    >
+      <revealer
+        revealChild={reveal}
+        transitionType={Gtk.RevealerTransitionType.CROSSFADE}
+        transitionDuration={300}
+      >
+        {wallpaper}
+      </revealer>
+    </window>
   )
 }
