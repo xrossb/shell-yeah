c0b1a0aefbaff446db9c46d2bd3358e7187e8413
Edward Allen <hi@eallen.me>
2025-11-20 18:34:39 +1100

    cleanup notification indicator

--- src/window/Bar/Item/NotificationIndicator.tsx
+++ src/window/Bar/Item/NotificationIndicator.tsx
@@ -1,35 +1,23 @@
 import AstalNotifd from "gi://AstalNotifd"
-import { createBinding, With } from "ags"
+import { createBinding } from "ags"
 import { Config } from "@/config"
 import BarItem from "../BarItem"
 
 const notifd = AstalNotifd.get_default()
 
 export default function NotificationIndicator() {
-  const notifications = createBinding(notifd, "notifications")
+  const count = createBinding(notifd, "notifications").as((n) => n.length)
+
+  const visible = count.as((c) => !!c)
+  const label = count.as((c) => (c > 99 ? "99+" : c.toString()))
+  const icon = count.as((c) =>
+    c ? "sy-notifications-unread-symbolic" : "sy-notifications-symbolic"
+  )
 
   return (
     <BarItem spacing={Config.spacing.small}>
-      <image
-        iconName={notifications((n) =>
-          n.length
-            ? "sy-notifications-unread-symbolic"
-            : "sy-notifications-symbolic"
-        )}
-        pixelSize={Config.sizing.indicatorIcon}
-      />
-      <With value={notifications}>
-        {(notifications) => (
-          <label
-            visible={!!notifications.length}
-            label={
-              notifications.length > 99
-                ? "99+"
-                : notifications.length.toString()
-            }
-          />
-        )}
-      </With>
+      <image iconName={icon} pixelSize={Config.sizing.indicatorIcon} />
+      <label visible={visible} label={label} />
     </BarItem>
   )
 }
