5668d1b06e7099894562ee16c601d979653015d5
Edward Allen <hi@eallen.me>
2026-04-06 23:33:42 +1000

    start doing some audio menu shenanigans

--- src/components/baritems/Audio.tsx
+++ src/components/baritems/Audio.tsx
@@ -1,10 +1,16 @@
 import BarItem from "@/src/components/BarItem"
 import Icon from "../Icon"
+import { Gdk, Gtk } from "ags/gtk4"
+import app from "ags/gtk4/app"
 
 export default function Audio() {
   return (
     <BarItem>
       <Icon icon="sy-volume-high" />
+      <Gtk.GestureClick
+        button={Gdk.BUTTON_PRIMARY}
+        onPressed={() => app.toggle_window("audio-menu")}
+      />
     </BarItem>
   )
 }
--- src/windows/AudioMenu.tsx
+++ src/windows/AudioMenu.tsx
@@ -1,7 +1,27 @@
+import { Astal, Gtk } from "ags/gtk4"
+import Popup from "../components/Popup"
+
+const name = "audio-menu"
+
 /**
  * Audio settings modal.
  */
 export default function AudioMenu() {
-  // TODO
-  return <></>
+  const { TOP, RIGHT } = Astal.WindowAnchor
+  return (
+    <Popup
+      name={name}
+      namespace={name}
+      width={300}
+      anchor={TOP | RIGHT}
+      margin={8}
+    >
+      <box orientation={Gtk.Orientation.VERTICAL}>
+        <Gtk.DropDown model={Gtk.StringList.new(["hello", "world"])} />
+        <slider />
+        <Gtk.DropDown model={Gtk.StringList.new(["hello", "world"])} />
+        <slider />
+      </box>
+    </Popup>
+  )
 }
