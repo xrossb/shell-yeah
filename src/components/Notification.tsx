fd0525aea7995d057dd66f9da0c049b2846a3fb4
Edward Allen <hi@eallen.me>
2026-04-06 15:18:06 +1000

    notification popups work?

---
+++ src/components/Notification.tsx
@@ -0,0 +1,132 @@
+import { Accessor, createState, Node } from "ags"
+import { Gdk, Gtk } from "ags/gtk4"
+import Adw from "gi://Adw?version=1"
+import AstalNotifd from "gi://AstalNotifd?version=0.1"
+import Gio from "gi://Gio?version=2.0"
+import GLib from "gi://GLib?version=2.0"
+import Pango from "gi://Pango?version=1.0"
+
+export type Props = {
+  notification: AstalNotifd.Notification
+  onDismiss: () => void
+}
+
+export default function Notification({ notification, onDismiss }: Props) {
+  const { VERTICAL } = Gtk.Orientation
+
+  const [closeVisible, setCloseVisible] = createState(false)
+
+  return (
+    <Adw.Clamp maximumSize={400} class="notification">
+      <overlay>
+        <Gtk.EventControllerMotion
+          onEnter={() => setCloseVisible(true)}
+          onLeave={() => setCloseVisible(false)}
+        />
+        <box orientation={VERTICAL} widthRequest={400} spacing={8}>
+          <box spacing={8}>
+            {NotificationIcon(notification)}
+            <box orientation={VERTICAL}>
+              {Header(notification)}
+              {TextContent(notification)}
+            </box>
+          </box>
+          {Actions(notification)}
+        </box>
+        {Close(closeVisible, onDismiss)}
+      </overlay>
+    </Adw.Clamp>
+  )
+}
+
+function NotificationIcon(n: AstalNotifd.Notification) {
+  function clamp(children: Node) {
+    return (
+      <Adw.Clamp maximumSize={32} valign={Gtk.Align.START}>
+        {children}
+      </Adw.Clamp>
+    )
+  }
+
+  if (n.image && isFile(n.image)) {
+    return clamp(
+      <Gtk.Picture
+        contentFit={Gtk.ContentFit.CONTAIN}
+        file={Gio.file_new_for_path(n.image)}
+      />,
+    )
+  }
+
+  const icon = n.appIcon || n.desktopEntry
+  if (isIcon(icon)) {
+    return clamp(<image iconName={icon} pixelSize={32} />)
+  }
+}
+
+function Header(n: AstalNotifd.Notification) {
+  const time = GLib.DateTime.new_from_unix_local(n.time).format("%I:%M %P")
+  const summary = n.summary.replace(n.appName, "")
+  const parts = [time, n.appName, summary].filter((x) => x)
+  return (
+    <label
+      class="header"
+      halign={Gtk.Align.START}
+      ellipsize={Pango.EllipsizeMode.END}
+      label={parts.join(" · ")}
+    />
+  )
+}
+
+function TextContent(n: AstalNotifd.Notification) {
+  return (
+    <label
+      class="body"
+      visible={!!n.body}
+      halign={Gtk.Align.FILL}
+      xalign={0}
+      wrap
+      wrapMode={Pango.WrapMode.WORD_CHAR}
+      label={n.body}
+    />
+  )
+}
+
+function Actions(n: AstalNotifd.Notification) {
+  const actions = n.actions.filter((action) => action.id !== "default")
+  return (
+    <box class="actions" visible={!!actions.length} spacing={8} homogeneous>
+      {actions.map((action) => (
+        <button onClicked={() => n.invoke(action.id)}>
+          <label label={action.label} />
+        </button>
+      ))}
+    </box>
+  )
+}
+
+function Close(visible: Accessor<boolean>, onClick: () => void) {
+  return (
+    <button
+      $type="overlay"
+      class="close"
+      visible={visible}
+      onClicked={onClick}
+      halign={Gtk.Align.END}
+      valign={Gtk.Align.START}
+    >
+      <image pixelSize={16} iconName="sy-close" />
+    </button>
+  )
+}
+
+let iconTheme: Gtk.IconTheme
+function isIcon(name: string) {
+  if (!iconTheme) {
+    iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default()!)
+  }
+  return !!name && iconTheme.has_icon(name)
+}
+
+function isFile(path: string) {
+  return GLib.file_test(path, GLib.FileTest.EXISTS)
+}
--- src/windows/Notifications.tsx
+++ src/windows/Notifications.tsx
@@ -1,15 +1,19 @@
-import { createBinding, createComputed, createState, For, onCleanup } from "ags"
+import {
+  Accessor,
+  createBinding,
+  createComputed,
+  createState,
+  For,
+  Node,
+  onCleanup,
+} from "ags"
+import { Astal, Gtk } from "ags/gtk4"
 import AstalNotifd from "gi://AstalNotifd?version=0.1"
-import Window from "../components/Window"
-import { Astal, Gdk, Gtk } from "ags/gtk4"
-import Pango from "gi://Pango?version=1.0"
-import Gio from "gi://Gio?version=2.0"
-import Adw from "gi://Adw?version=1"
-import AstalApps from "gi://AstalApps?version=0.1"
 import GLib from "gi://GLib?version=2.0"
-import app from "ags/gtk4/app"
+import Notification from "../components/Notification"
+import Window from "../components/Window"
 
-const name = "notifications_menu"
+const name = "notifications-popup"
 
 /**
  * Display for incoming notifications.
@@ -51,205 +55,51 @@ export default function Notifications() {
       resizable={false}
     >
       <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
-        <For each={notifications}>{Notification}</For>
+        <For each={notifications}>
+          {(notification) =>
+            PopupNotification(notification, (hide) =>
+              setNotifications((ns) =>
+                ns.filter((existing) => existing.id !== hide.id),
+              ),
+            )
+          }
+        </For>
       </box>
     </Window>
   )
 }
 
-function Notification(n: AstalNotifd.Notification) {
-  const { VERTICAL } = Gtk.Orientation
+function PopupNotification(
+  notification: AstalNotifd.Notification,
+  onHide: (notification: AstalNotifd.Notification) => void,
+) {
+  let timeout: GLib.Source
+  let resumeTime: number
+  let timeLeft = 5000
+
+  function pause() {
+    timeout.destroy()
+    timeLeft -= Date.now() - resumeTime
+  }
+
+  function resume() {
+    timeout = setTimeout(() => {
+      onHide(notification)
+    }, timeLeft)
+    resumeTime = Date.now()
+  }
+
+  resume()
   return (
-    <box orientation={VERTICAL}>
-      <box>
-        {AppIcon(n)}
-        <box orientation={VERTICAL}>
-          {Header(n)}
-          {TextContent(n)}
-        </box>
-      </box>
-      {ImageContent(n)}
-      {Actions(n)}
+    <box>
+      <Gtk.EventControllerMotion
+        onEnter={() => pause()}
+        onLeave={() => resume()}
+      />
+      <Notification
+        notification={notification}
+        onDismiss={() => notification.dismiss()}
+      />
     </box>
   )
 }
-
-function AppIcon(n: AstalNotifd.Notification) {
-  const icon = n.appIcon || n.desktopEntry
-
-  if (isFile(icon)) {
-    return (
-      <Adw.Clamp maximumSize={32}>
-        <Gtk.Picture
-          contentFit={Gtk.ContentFit.CONTAIN}
-          file={Gio.file_new_for_path(n.image)}
-        />
-      </Adw.Clamp>
-    )
-  }
-
-  return isIcon(icon) && <image iconName={icon} iconSize={Gtk.IconSize.LARGE} />
-}
-
-function Header(n: AstalNotifd.Notification) {
-  const parts = [n.time, n.appName, n.summary].filter((x) => x)
-  return <label ellipsize={Pango.EllipsizeMode.END} label={parts.join(" · ")} />
-}
-
-function TextContent(n: AstalNotifd.Notification) {
-  return (
-    <label
-      visible={!!n.body}
-      halign={Gtk.Align.START}
-      wrap
-      wrapMode={Pango.WrapMode.WORD_CHAR}
-      label={n.body}
-    />
-  )
-}
-
-function ImageContent(n: AstalNotifd.Notification) {
-  return isFile(n.image) && <image file={n.image} />
-}
-
-function Actions(n: AstalNotifd.Notification) {
-  const actions = n.actions.filter((action) => action.id !== "default")
-  return (
-    <box visible={!!actions.length}>
-      {actions.map((action) => (
-        <button onClicked={() => n.invoke(action.id)}>
-          <label hexpand label={action.label} />
-        </button>
-      ))}
-    </box>
-  )
-}
-
-let iconTheme: Gtk.IconTheme
-function isIcon(name: string) {
-  if (!iconTheme) {
-    iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default()!)
-  }
-  return !!name && iconTheme.has_icon(name)
-}
-
-function isFile(path: string) {
-  return GLib.file_test(path, GLib.FileTest.EXISTS)
-}
-
-// function Notification(n: AstalNotifd.Notification) {
-//   const classes = ["notification"]
-//   if (n.urgency === AstalNotifd.Urgency.CRITICAL) {
-//     classes.push("urgent")
-//   }
-
-//   const [closeVisible, setCloseVisible] = createState(false)
-
-//   return (
-//     <overlay cssClasses={classes}>
-//       <Gtk.EventControllerMotion
-//         onEnter={() => setCloseVisible(true)}
-//         onLeave={() => setCloseVisible(false)}
-//       />
-//       <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
-//         {n.actions.length === 1 && (
-//           <Gtk.GestureClick
-//             button={Gdk.BUTTON_PRIMARY}
-//             onReleased={() => n.actions[0].invoke()}
-//           />
-//         )}
-//         <box spacing={8}>
-//           {NotificationIcon(n)}
-//           <box orientation={Gtk.Orientation.VERTICAL} heightRequest={30}>
-//             {n.appName && (
-//               <label
-//                 class="header"
-//                 halign={Gtk.Align.START}
-//                 label={n.appName}
-//                 ellipsize={Pango.EllipsizeMode.END}
-//               />
-//             )}
-//             {n.summary && (
-//               <label
-//                 class="header"
-//                 halign={Gtk.Align.START}
-//                 label={n.summary}
-//                 ellipsize={Pango.EllipsizeMode.END}
-//               />
-//             )}
-//             <label xalign={0} label={n.body} wrap />
-//           </box>
-//         </box>
-//         {n.actions.length > 1 && (
-//           <box spacing={8}>
-//             {n.actions.map((action) => (
-//               <button
-//                 label={action.label}
-//                 onClicked={() => action.invoke()}
-//                 hexpand
-//               />
-//             ))}
-//           </box>
-//         )}
-//       </box>
-//       {/* <image
-//         $type="overlay"
-//         visible={closeVisible}
-//         pixelSize={Config.icon.size.indicator}
-//         iconName="sy-close-symbolic"
-//         halign={Gtk.Align.END}
-//         valign={Gtk.Align.START}
-//         class="icon-button"
-//       >
-//         <Gtk.GestureClick
-//           button={Gdk.BUTTON_PRIMARY}
-//           onReleased={() => n.dismiss()}
-//         />
-//       </image> */}
-//     </overlay>
-//   )
-// }
-
-// function NotificationIcon(n: AstalNotifd.Notification) {
-//   const apps = new AstalApps.Apps()
-
-//   if (n.image && isFile(n.image)) {
-//     return (
-//       <Adw.Clamp maximumSize={32} valign={Gtk.Align.START}>
-//         <Gtk.Picture
-//           class={"image"}
-//           contentFit={Gtk.ContentFit.CONTAIN}
-//           file={Gio.file_new_for_path(n.image)}
-//         />
-//       </Adw.Clamp>
-//     )
-//   }
-
-//   if (n.appIcon) {
-//     const icon = Gio.Icon.new_for_string(n.appIcon)
-//     return <image gicon={icon} pixelSize={32} valign={Gtk.Align.START} />
-//   }
-
-//   const id = n.desktopEntry || n.appName
-//   if (!id) {
-//     return
-//   }
-
-//   const app = apps.list.find((app) => {
-//     return (
-//       app.entry?.toLowerCase() === id ||
-//       app.iconName?.toLowerCase() === id ||
-//       app.name?.toLowerCase() === id ||
-//       app.wmClass?.toLowerCase() === id
-//     )
-//   })
-
-//   if (app && app.iconName) {
-//     const icon = Gio.Icon.new_for_string(app.iconName)
-//     return <image gicon={icon} pixelSize={32} valign={Gtk.Align.START} />
-//   }
-// }
-
-// function isFile(path: string) {
-//   return GLib.file_test(path, GLib.FileTest.EXISTS)
-// }
