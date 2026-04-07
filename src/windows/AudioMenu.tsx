0e75af448812c9dd01ea180316842c25c6ca2b90
Edward Allen <hi@eallen.me>
2026-04-07 19:05:40 +1000

    audio player experiments

--- src/windows/AudioMenu.tsx
+++ src/windows/AudioMenu.tsx
@@ -1,7 +1,13 @@
 import { Astal, Gtk } from "ags/gtk4"
 import Popup from "../components/Popup"
 import Icon from "../components/Icon"
-import { Accessor } from "ags"
+import { Accessor, createBinding, For, With } from "ags"
+import AstalWp from "gi://AstalWp?version=0.1"
+import AstalMpris from "gi://AstalMpris?version=0.1"
+import Adw from "gi://Adw?version=1"
+import Gio from "gi://Gio?version=2.0"
+import AstalApps from "gi://AstalApps?version=0.1"
+import Pango from "gi://Pango?version=1.0"
 
 const name = "audio-menu"
 
@@ -10,6 +16,7 @@ const name = "audio-menu"
  */
 export default function AudioMenu() {
   const { TOP, RIGHT } = Astal.WindowAnchor
+
   return (
     <Popup
       name={name}
@@ -18,119 +25,220 @@ export default function AudioMenu() {
       anchor={TOP | RIGHT}
       margin={8}
     >
-      <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
-        <box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
-          <box spacing={8}>
-            <image iconName="sy-volume-high" />
-            <label
-              label="Output Device"
-              halign={Gtk.Align.START}
-              justify={Gtk.Justification.LEFT}
-              hexpand
-            />
-            <button iconName="sy-chevron-right" />
-          </box>
-          <slider hexpand />
-        </box>
-        <box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
-          <box spacing={8}>
-            <image iconName="sy-mic" />
-            <label
-              label="Generic Input"
-              halign={Gtk.Align.START}
-              justify={Gtk.Justification.LEFT}
-              hexpand
-            />
-            <button iconName="sy-chevron-right" />
-          </box>
-          <slider hexpand />
-        </box>
-        <Gtk.Separator />
-        <box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
-          <box spacing={8}>
-            <image iconName="sy-volume-high" />
-            <label label="Output Device" />
-          </box>
-          <box spacing={8}>
-            <slider hexpand />
-            <button iconName="sy-chevron-right" />
-          </box>
-        </box>
-        <box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
-          <box spacing={8}>
-            <image iconName="sy-mic" />
-            <label label="Generic Input" />
-          </box>
-          <box spacing={8}>
-            <slider hexpand />
-            <button iconName="sy-chevron-right" />
-          </box>
-        </box>
-        <Gtk.Separator />
-        <box spacing={8}>
-          <Slider icon="sy-volume-high" label="this is an output" />
-          <button iconName="sy-chevron-right" />
-        </box>
-        <box spacing={8}>
-          <Slider icon="sy-mic" label="input device" />
-          <button iconName="sy-chevron-right" />
-        </box>
-        <Gtk.Separator />
-        <box>
-          <button valign={Gtk.Align.CENTER}>
-            <image iconName="sy-volume-high" pixelSize={20} />
-          </button>
-          <slider hexpand />
-          <button valign={Gtk.Align.CENTER}>
-            <image iconName="sy-chevron-right" pixelSize={20} />
-          </button>
-        </box>
-        <box>
-          <button valign={Gtk.Align.CENTER}>
-            <image iconName="sy-mic" pixelSize={20} />
-          </button>
-          <slider hexpand />
-          <button valign={Gtk.Align.CENTER}>
-            <image iconName="sy-chevron-right" pixelSize={20} />
-          </button>
-        </box>
-        <Gtk.Separator />
-        <label label="volume + device pickers" />
-        <label label="media players" />
+      <box orientation={Gtk.Orientation.VERTICAL} spacing={16}>
+        <DeviceControls />
+        <PlayerList />
       </box>
     </Popup>
   )
 }
 
-type SliderProps = {
-  icon?: string | Accessor<string>
-  label?: string | Accessor<string>
-  level?: number | Accessor<number>
-  min?: number
-  max?: number
-  onChange?: (value: number) => void
+function DeviceControls() {
+  const wp = AstalWp.get_default()
+  const speaker = createBinding(wp, "defaultSpeaker")
+  const microphone = createBinding(wp, "defaultMicrophone")
+
+  return (
+    <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
+      <With value={speaker}>
+        {(speaker) => (
+          <DeviceSlider
+            endpoint={speaker}
+            icons={{ normal: "sy-volume-high", mute: "sy-volume-off" }}
+          />
+        )}
+      </With>
+      <With value={microphone}>
+        {(microphone) => (
+          <DeviceSlider
+            endpoint={microphone}
+            icons={{ normal: "sy-mic", mute: "sy-mic-off" }}
+          />
+        )}
+      </With>
+    </box>
+  )
 }
 
-function Slider({ icon, label, level, min, max, onChange }: SliderProps) {
+type SliderProps = {
+  endpoint: AstalWp.Endpoint
+  icons: {
+    normal: string
+    mute: string
+  }
+}
+
+function DeviceSlider({ endpoint, icons }: SliderProps) {
+  const volume = createBinding(endpoint, "volume")
+  const mute = createBinding(endpoint, "mute")
+
   return (
-    <overlay class="slider-box">
-      <box
-        $type="overlay"
-        halign={Gtk.Align.START}
-        valign={Gtk.Align.CENTER}
-        spacing={4}
-      >
-        <image iconName={icon} pixelSize={16} />
-        <label label={label} />
-      </box>
+    <box>
+      <button class="no-bg" onClicked={() => (endpoint.mute = !endpoint.mute)}>
+        <image
+          iconName={mute.as((m) => (m ? icons?.mute : icons?.normal))}
+          pixelSize={20}
+        />
+      </button>
       <slider
-        onChangeValue={({ value }) => onChange?.(value)}
-        value={level}
-        min={min}
-        max={max}
-        valign={Gtk.Align.FILL}
         hexpand
+        value={volume}
+        onChangeValue={({ value }) => {
+          endpoint.volume = value
+        }}
       />
+      <button class="no-bg">
+        <image iconName="sy-chevron-right" pixelSize={20} />
+      </button>
+    </box>
+  )
+}
+
+function PlayerList() {
+  const mpris = AstalMpris.get_default()
+  const players = createBinding(mpris, "players")
+
+  return <For each={players}>{(player) => <Player player={player} />}</For>
+}
+
+type PlayerProps = {
+  player: AstalMpris.Player
+}
+
+function Player({ player }: PlayerProps) {
+  return (
+    <overlay class="player" heightRequest={150}>
+      <PlayerArt player={player} />
+      <PlayerContent player={player} />
     </overlay>
   )
+}
+
+function PlayerArt({ player }: PlayerProps) {
+  const coverArt = createBinding(player, "coverArt").as((c) =>
+    Gio.file_new_for_path(c),
+  )
+
+  return (
+    <Adw.Clamp $type={"overlay"}>
+      <Gtk.Picture
+        class="art"
+        file={coverArt}
+        contentFit={Gtk.ContentFit.COVER}
+      />
+    </Adw.Clamp>
+  )
+}
+
+function PlayerContent({ player }: PlayerProps) {
+  const apps = new AstalApps.Apps()
+  const title = createBinding(player, "title").as((t) => t || "Unknown Track")
+  const artist = createBinding(player, "artist").as(
+    (a) => a || "Unknown Artist",
+  )
+  const playIcon = createBinding(player, "playbackStatus").as((s) =>
+    s === AstalMpris.PlaybackStatus.PLAYING
+      ? "sy-player-pause"
+      : "sy-player-play",
+  )
+  const app = apps.list.find((a) => a.entry === player.entry)
+
+  return (
+    <box
+      class="content"
+      $type="overlay"
+      orientation={Gtk.Orientation.VERTICAL}
+      spacing={8}
+    >
+      <box>
+        <box
+          orientation={Gtk.Orientation.VERTICAL}
+          spacing={4}
+          valign={Gtk.Align.START}
+        >
+          <label
+            label={title}
+            hexpand
+            halign={Gtk.Align.START}
+            ellipsize={Pango.EllipsizeMode.END}
+            maxWidthChars={30}
+          />
+          <label
+            label={artist}
+            halign={Gtk.Align.START}
+            ellipsize={Pango.EllipsizeMode.END}
+            maxWidthChars={35}
+          />
+        </box>
+        <box hexpand />
+        <box spacing={4} valign={Gtk.Align.START}>
+          <image iconName={app?.iconName} pixelSize={20} />
+          <label label={player.identity} />
+        </box>
+      </box>
+      <box vexpand />
+      <box>
+        <box
+          visible={createBinding(player, "length").as((l) => l > 0)}
+          valign={Gtk.Align.END}
+        >
+          <label
+            cssClasses={["position"]}
+            label={createBinding(player, "position").as((l) =>
+              l > 0 ? lengthStr(l) : "0:00",
+            )}
+          />
+          <label label={" / "} />
+          <label
+            cssClasses={["length"]}
+            label={createBinding(player, "length").as((l) =>
+              l > 0 ? lengthStr(l) : "0:00",
+            )}
+          />
+        </box>
+        <box hexpand />
+        <box class={"buttons"} spacing={8} valign={Gtk.Align.END}>
+          <button
+            class="no-bg"
+            onClicked={() => player.previous()}
+            focusOnClick={false}
+            visible={createBinding(player, "canGoPrevious")}
+          >
+            <image iconName="sy-player-prev" pixelSize={20} />
+          </button>
+          <button
+            class="no-bg"
+            onClicked={() => player.play_pause()}
+            focusOnClick={false}
+            visible={createBinding(player, "canControl")}
+          >
+            <image iconName={playIcon} pixelSize={20} />
+          </button>
+          <button
+            class="no-bg"
+            onClicked={() => player.next()}
+            focusOnClick={false}
+            visible={createBinding(player, "canGoNext")}
+          >
+            <image iconName="sy-player-next" pixelSize={20} />
+          </button>
+        </box>
+      </box>
+    </box>
+  )
+}
+
+function lengthStr(length: number): string {
+  const hours = Math.floor(length / 3600)
+  const minutes = Math.floor((length % 3600) / 60)
+  const seconds = Math.floor(length % 60)
+
+  const formatTime = (value: number): string =>
+    value < 10 ? `0${value}` : `${value}`
+
+  if (hours > 0) {
+    return `${hours}:${formatTime(minutes)}:${formatTime(seconds)}`
+  }
+
+  return `${minutes}:${formatTime(seconds)}`
 }
