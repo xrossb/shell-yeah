296889387c47cd188913c01dd6b6bb0e0833cdac
Edward Allen <hi@eallen.me>
2026-04-07 14:56:31 +1000

    revert Gio.Subprocess usage; has same effect as .launch

--- src/lib/search/apps.ts
+++ src/lib/search/apps.ts
@@ -1,7 +1,5 @@
 import * as search from "@/src/lib/search"
 import AstalApps from "gi://AstalApps?version=0.1"
-import Gio from "gi://Gio?version=2.0"
-import GLib from "gi://GLib?version=2.0"
 
 /**
  * Search plugin which returns installed applications.
@@ -23,12 +21,7 @@ export class Plugin implements search.Plugin {
         apps[i].name,
         apps[i].description || apps[i].entry,
         (ctx) => {
-          Gio.Subprocess.new(
-            GLib.shell_parse_argv(
-              apps[i].executable.replace(/%[fFcuUik]/g, ""),
-            )[1]!,
-            Gio.SubprocessFlags.NONE,
-          )
+          apps[i].launch()
           ctx.close()
         },
       )
