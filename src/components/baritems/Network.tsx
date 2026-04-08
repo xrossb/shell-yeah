542b7b39813bbe39e6a37d6c44e35878f3bb10df
Edward Allen <hi@eallen.me>
2026-04-08 15:08:47 +1000

    implement network bar icon

--- src/components/baritems/Network.tsx
+++ src/components/baritems/Network.tsx
@@ -1,10 +1,51 @@
 import BarItem from "@/src/components/BarItem"
-import Icon from "../Icon"
+import AstalNetwork from "gi://AstalNetwork?version=0.1"
+import { createBinding, createComputed, With } from "ags"
 
 export default function Network() {
+  const network = AstalNetwork.get_default()
+  const wifi = createBinding(network, "wifi")
+  const wired = createBinding(network, "wired")
+  const visible = createComputed(() => !!(wifi() || wired()))
+
   return (
-    <BarItem>
-      <Icon icon="sy-wifi-strong" />
+    <BarItem visible={visible}>
+      <With value={wifi}>{(wifi) => wifi && <WifiIcon wifi={wifi} />}</With>
+      <With value={wired}>
+        {(wired) => wired && <WiredIcon wired={wired} />}
+      </With>
     </BarItem>
   )
+}
+
+type WifiProps = {
+  wifi: AstalNetwork.Wifi
+}
+
+function WifiIcon({ wifi }: WifiProps) {
+  const enabled = createBinding(wifi, "enabled")
+  const connected = createBinding(wifi, "activeConnection").as((c) => !!c)
+  const strength = createBinding(wifi, "strength")
+  const icon = createComputed(() => {
+    if (!enabled()) return "sy-wifi-off"
+    if (strength() <= 25) return "sy-wifi-weak"
+    if (strength() <= 50) return "sy-wifi-mid"
+    return "sy-wifi-strong"
+  })
+  const opacity = createComputed(() => {
+    if (enabled() && !connected()) return 0.5
+    return 1
+  })
+
+  return <image iconName={icon} opacity={opacity} />
+}
+
+type WiredProps = {
+  wired: AstalNetwork.Wired
+}
+
+function WiredIcon({ wired }: WiredProps) {
+  const connected = createBinding(wired, "connection").as((c) => !!c)
+  const opacity = connected.as((c) => (c ? 1 : 0.5))
+  return <image iconName="sy-lan" opacity={opacity} />
 }
