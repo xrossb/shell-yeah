2d89b14d358097a6370e367c4213c2bfd02c1239
Edward Allen <hi@eallen.me>
2026-04-15 18:20:57 +1000

    split off shell.rs, wip clock widget for bar

---
+++ src/shell.rs
@@ -0,0 +1,40 @@
+use gtk4_layer_shell::{Layer, LayerShell};
+use relm4::{gtk::prelude::*, prelude::*};
+
+use crate::modules::bar;
+
+pub struct Model {
+    bar: Controller<bar::Model>,
+}
+
+#[relm4::component(pub)]
+impl SimpleComponent for Model {
+    type Init = ();
+    type Input = ();
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_decorated: false,
+            set_opacity: 0.0,
+            set_layer: Layer::Background,
+        },
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        root.init_layer_shell();
+
+        let bar = bar::Model::builder()
+            .launch(bar::Init::default())
+            .forward(sender.input_sender(), |msg| msg);
+
+        let model = Model { bar };
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+}
